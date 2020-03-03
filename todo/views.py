70078570aaee62bb9c27f5ddc46bfd9e3691bd2c
SSep <ssep4u@gmail.com>
2020-03-03 19:01:33 +0900

    todo/views todo_fetch, todo_save

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import TodoForm
+from .models import Todo
+
+
+def todo_fetch(request):
+  todos = Todo.objects.all()
+  todo_list = []
+  for index,todo in enumerate(todos, start=1):
+    todo_list.append({'id':index, 'title':todo.title, 'completed':todo.completed})
+
+  return JsonResponse(todo_list, safe=False)
+
+@csrf_exempt
+def todo_save(request):
+  if request.body:
+    data = json.loads(request.body)
+    if 'todos' in data:
+      todos = data['todos']
+      Todo.objects.all().delete()
+      for todo in todos:
+        print('todo', todo)
+        form = TodoForm(todo)
+        if form.is_valid():
+          form.save()
+  return JsonResponse({})
